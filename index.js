1f6c5dcc4b316a674f6672b6ec57d121cb04036a
Espen K. Brun <espen_brun@hotmail.com>
2017-04-21 11:18:46 +0200

    In process of manually merging, replicate files

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-//////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 // Hyttekatalog logikk
-// Espen Kirkesæther Brun og Anna Nikitina
-//////////////////////////////////////////
+// Espen Kirkesæther Brun, Anna Nikitina og Kathinka Hoyden
+///////////////////////////////////////////////////////////////
 
-$(document).ready(function(){
+$(document).ready(function(){$("#katalog").click(function(){
 	var txt = "";
+	var hyttetype = this.name;
 	console.log(this);
+
 	$.ajax({
 		type:"GET",
-		url:"hyttekatalog.xml",
+		url:"hyttekatalog4.xml",
 		dataType: "xml",
+
 		success: function(xml) {
-			$(xml).find('fjellhytta').each(function(){
-				txt += $(this).find("beliggenhet").text() + 
-				$(this).find("utleieperioden").text() +
-			    $(this).find("kjøkken").text() +
-			    $(this).find("strøm").text() +
-			    $(this).find("innlagtvann").text() +
-			    $(this).find("dusj").text() +
-			    $(this).find("antallsenger").text(); 
+			$(xml).find(hyttetype).each(function(){
+				txt += "<hr>" + "<p><img src='" + $(this).find("bilde").text()+ "' height = 50 width = 40 ></p>" + "<br>" + 
+				"Beliggenhet: " + $(this).find("beliggenhet").text() + "<br>" +
+				"Utleieperioden: " + $(this).find("utleieperioden").text()  + "<br>" +
+			    "Kjøkken: " + $(this).find("kjøkken").text() + "<br>" +
+			    "Innlagt strøm: " + $(this).find("strøm").text() +   "<br>" +
+			    "Innlagt vann: " + $(this).find("innlagtvann").text()  + "<br>" +
+			    "Dusj: " + $(this).find("dusj").text() +  "<br>" +
+			    "Antall senger: " + $(this).find("antallsenger").text() + "<br>" +
+			    "Pris: " + $(this).find("pris").text() + "<br>" +
+			    "her er linken" +
+			    "<a href='hyttebeskrivelse.html' id='" + $(this).find("hytteid").text() + "'>Vil du vite mer?</a>" +
+			     "<hr>"; 
 			});
-			$(".main").append(txt);
+			$("#hytteliste").html(txt);
 		}
 	});
+  });
 });
+					
