ec6717f03c6e8f9ca80f28656049ab2aa4ce6571
Espen K. Brun <espen_brun@hotmail.com>
2017-04-21 14:02:39 +0200

    La til siste endringer fra Anna. Ordnet linkfarge på forsiden. Alle linker på forsiden fungerer. h1 på hytteliste hentes fra xml

--- hytteliste.js
+++ hytteliste.js
@@ -4,9 +4,13 @@
 ///////////////////////////////////////////////////////////////
 
 $(document).ready(function(){
+	// Hent informasjon om hvilken type hytter som skal vises fra enden av urlen
 	var hyttetype = window.location.hash.substring(1);
 	var txt = "";
-		
+	var header = hyttetype;
+	$("#cabin-type-header").html(hyttetype);
+	
+	// Hent informasjon om hyttene
 	$.ajax({
 		type:"GET",
 		url:"hyttekatalog.xml",
@@ -26,6 +30,7 @@ $(document).ready(function(){
 			    "her er linken" +
 			    "<a href='hyttebeskrivelse.html' id='" + $(this).find("hytteid").text() + "'>Vil du vite mer?</a>" + "<hr>"; 
 			});
+			// Send HTMLen til dette elementet
 			$("#hytteliste").html(txt);
 		}
 	});
--- index.js
+++ index.js
@@ -3,40 +3,16 @@
 // Espen Kirkesæther Brun, Anna Nikitina og Kathinka Hoyden
 ///////////////////////////////////////////////////////////////
 
+// for senere bruk. Legge tilk mulighet for å skifte hyttetype når man er inne i hyttelsite.html
+// $(document).delegate('#katalog','click', function(){} 
+
+
 $(document).ready(function(){
-	$("#katalog").click(function(e){
-		e.preventDefault();
+	// click for hyttekategori. Hent info om hvilken type som er klikket, og send med denne informasjonen i linken, og send til hytteliste.html
+	$(".goto-cabin").click(function(e){
+		e.preventDefault(); // hindre default oppførsel for browseren ved klikk
 		var hyttetype = this.name;
 		window.location.href = 'hytteliste.html#'+hyttetype;
-
 	});
-	// $("#katalog").click(function(){
-	// 	var txt = "";
-	// 	var hyttetype = this.name;
-	// 	console.log(this);
-
-	// 	$.ajax({
-	// 		type:"GET",
-	// 		url:"hyttekatalog.xml",
-	// 		dataType: "xml",
-
-	// 		success: function(xml) {
-	// 			$(xml).find(hyttetype).each(function(){
-	// 				txt += "<hr>" + "<p><img src='" + $(this).find("bilde").text()+ "' height = 50 width = 40 ></p>" + "<br>" + 
-	// 				"Beliggenhet: " + $(this).find("beliggenhet").text() + "<br>" +
-	// 				"Utleieperioden: " + $(this).find("utleieperioden").text()  + "<br>" +
-	// 			    "Kjøkken: " + $(this).find("kjøkken").text() + "<br>" +
-	// 			    "Innlagt strøm: " + $(this).find("strøm").text() +   "<br>" +
-	// 			    "Innlagt vann: " + $(this).find("innlagtvann").text()  + "<br>" +
-	// 			    "Dusj: " + $(this).find("dusj").text() +  "<br>" +
-	// 			    "Antall senger: " + $(this).find("antallsenger").text() + "<br>" +
-	// 			    "Pris: " + $(this).find("pris").text() + "<br>" +
-	// 			    "her er linken" +
-	// 			    "<a href='hyttebeskrivelse.html' id='" + $(this).find("hytteid").text() + "'>Vil du vite mer?</a>" + "<hr>"; 
-	// 			});
-	// 			$("#hytteliste").html(txt);
-	// 		}
-	// 	});
- //    });
 });
 					
