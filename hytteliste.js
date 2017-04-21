f908b0e283471679b7b6764a37a3af71082aa230
Espen K. Brun <espen_brun@hotmail.com>
2017-04-21 12:51:50 +0200

    hyttekategori linker til ny side som henter ut riktig info. Kun for fjell foreløpig

---
+++ hytteliste.js
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////
+// Hytteliste logikk
+// Espen Kirkesæther Brun, Anna Nikitina og Kathinka Hoyden
+///////////////////////////////////////////////////////////////
+
+$(document).ready(function(){
+	var hyttetype = window.location.hash.substring(1);
+	var txt = "";
+		
+	$.ajax({
+		type:"GET",
+		url:"hyttekatalog.xml",
+		dataType: "xml",
+
+		success: function(xml) {
+			$(xml).find(hyttetype).each(function(){
+				txt += "<hr>" + "<p><img src='" + $(this).find("bilde").text()+ "' height = 50 width = 40 ></p>" + "<br>" + 
+				"Beliggenhet: " + $(this).find("beliggenhet").text() + "<br>" +
+				"Utleieperioden: " + $(this).find("utleieperioden").text()  + "<br>" +
+			    "Kjøkken: " + $(this).find("kjøkken").text() + "<br>" +
+			    "Innlagt strøm: " + $(this).find("strøm").text() +   "<br>" +
+			    "Innlagt vann: " + $(this).find("innlagtvann").text()  + "<br>" +
+			    "Dusj: " + $(this).find("dusj").text() +  "<br>" +
+			    "Antall senger: " + $(this).find("antallsenger").text() + "<br>" +
+			    "Pris: " + $(this).find("pris").text() + "<br>" +
+			    "her er linken" +
+			    "<a href='hyttebeskrivelse.html' id='" + $(this).find("hytteid").text() + "'>Vil du vite mer?</a>" + "<hr>"; 
+			});
+			$("#hytteliste").html(txt);
+		}
+	});
+});
+					
