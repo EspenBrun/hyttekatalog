c465ab0ae17cc7ed1e86b6028baff55b9e7bb3bd
Espen K. Brun <espen_brun@hotmail.com>
2017-05-09 22:11:10 +0200

    js sjekk av skjema

--- skjema_bestilling.js
+++ skjema_bestilling.js
@@ -1,9 +1,3 @@
-function validerSkjema(){
-  var epost = window.document.mittSkjema.epost.value;
-  if (epost.value == "") {
-    alert("skriv inn gyldig epostadresse")
-    window.document.mittSkjema.epost.focus();
-    return false;
-}
-return true;
-}
+$(document).ready(function(){
+	
+});
---
+++ valider_skjema.js
@@ -0,0 +1,45 @@
+function validerSkjema(){
+	var container = $("#container-message");
+	var name = $("[name=navn]").val();
+	var adresse = $("[name=adresse]").val();
+	var postnrInput = $("[name=postnr]").val();
+	var postnr = parseInt(postnrInput) || 0; //if parseInt doesn't work, set postnr to 0
+	var epost = $("[name=epost]").val();
+	var tlfInput = $("[name=tlf]").val();
+	var tlf = parseInt(tlfInput) || 0;
+	var dager = $("[name=dager]").val();
+	var medlem = $("[name=medlem]").is(':checked');
+
+	if(name == ""){
+		container.text("Du må oppgi et navn");
+		container.show();
+		return false
+	}
+	if(adresse == "" || !adresse.includes(" ")){
+		container.text("Du må oppgi en adresse");
+		container.show();
+		return false
+	}
+	if(postnr == 0 || postnr < 1000 || postnr > 9999){
+		container.text("Du må oppgi et postnummer");
+		container.show();
+		return false
+	}
+	if(epost == "" || !epost.includes("@") || !epost.includes(".")){
+		container.text("Du må oppgi en epost");
+		container.show();
+		return false
+	}
+	if(tlf == 0 || tlf < 10000000 || tlf > 99999999){
+		container.text("Du må oppgi et telefonnummer");
+		container.show();
+		return false
+	}
+	if (dager <= 0) {
+		container.text("Antall dager må være positivt");
+		container.show();
+		return false	
+	}
+
+	return true;
+}
