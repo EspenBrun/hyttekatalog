8d6f1d8f3e36fdfa8ca331c40acde726cd1d9f84
Espen K. Brun <espen_brun@hotmail.com>
2017-05-11 20:47:21 +0200

    Finally merged everything. eihytte complete, except styling

--- eihytte.js
+++ eihytte.js
@@ -22,93 +22,92 @@ $(document).ready(function(){
 	// Hent informasjon om hytta
 	function searchXML(){
 		$.ajax({
-			type:"GET",
-			url:"hyttekatalog.xml",
-			dataType: "xml",
-			success: function(xml) {
-				//finner hytteid value i xml
-				myXML = $(xml).find(hyttetype).filter(function(){		
-				 return	$(this).find("hytteid").text()==id;		 
-				});
-			
-				//lagrer hytte-info i display variabelen
-				txt = myXML.children().map(function(){
-				return $(this).text() + ',';		
-				}).get().join(' ');
-						
-				txtArr = txt.split(',');
-				console.log('txtArr:' + txtArr);
-				info = txtArr[10].toString().trim();
-				console.log('info:' + info);
-				vaer = txtArr[14].toString().trim();
-				console.log('varet:' + varet);
-				tur = txtArr[15].toString().trim();
-				console.log('tur:' + tur);
-								
-				varet = '\n'+ '<iframe src="'+ vaer +'" width="468" height="290" frameborder="0" style="margin: 10px 0 10px 0" scrolling="no"></iframe>\n';
-		 		console.log('vaer:' + vaer);
-
-				$("#beskrivelse").load(info);
-				$("#varet").append(varet);
-
-				$(".tur").click(function(e){
-					e.preventDefault(); // hindre default oppførsel for browseren ved klikk
-					window.location.href = tur;
-				});
-				//dp
-
-				events = [ 
-				    { Title: "Opptat", Date: new Date("05/05/2017") }, 
-				    { Title: "Stengt", Date: new Date("03/05/2017") }
-				];
-
-
-				$("#datepicker").datepicker({
-
-
-					beforeShowDay: function(date) {
-					    var result = [true, '', null];
-					    var matching = $.grep(events, function(event) {
-					        return event.Date.valueOf() === date.valueOf();
-					    });
-
-					    if (matching.length) {
-					        result = [true, 'highlight', null];
-					    }
-					    return result;
-					}/*,
-
-					onSelect: function(dateText) {
-					    var date,
-					        selectedDate = new Date(dateText),
-					        i = 0,
-					        event = null;*/
-
-					    /* Determine if the user clicked an event: */
-					 /*   while (i < events.length && !event) {
-					        date = events[i].Date;
-
-					        if (selectedDate.valueOf() === date.valueOf()) {
-					            event = events[i];
-					        }
-					        i++;
-					    }
-					    if (event) {
-					        /* If the event is defined, perform some action here; show a tooltip, navigate to a URL, etc. */
-					    /*    alert(event.Title);
-					    }
-
-					}*/
-				}); 
-
-				//enddp
-			}
+		type:"GET",
+		url:"hyttekatalog.xml",
+		dataType: "xml",
+		success: function(xml) {
+		//finner hytteid value i xml
+		myXML = $(xml).find(hyttetype).filter(function(){		
+		 return	$(this).find("hytteid").text()==id;		 
 		});
-	}
+	
+		//lagrer hytte-info i display variabelen
+		txt = myXML.children().map(function(){
+		return $(this).text() + ',';		
+		}).get().join(' ');
+				
+		txtArr = txt.split(',');
+		console.log('txtArr:' + txtArr);
+		info = txtArr[10].toString().trim();
+		console.log('info:' + info);
+		vaer = txtArr[14].toString().trim();
+		console.log('varet:' + varet);
+		tur = txtArr[15].toString().trim();
+		console.log('tur:' + tur);
+						
+		varet = '\n'+ '<iframe src="'+ vaer +'" width="468" height="290" frameborder="0" style="margin: 10px 0 10px 0" scrolling="no"></iframe>\n';
+ 		console.log('vaer:' + vaer);
 
-	searchXML();
+	$("#beskrivelse").load(info);
+		$("#varet").append(varet);
 
-	// Make the gooogle maps map
+$(".tur").click(function(e){
+		e.preventDefault(); // hindre default oppførsel for browseren ved klikk
+		window.location.href = tur;
+});
+//dp
+
+events = [ 
+    { Title: "Opptat", Date: new Date("05/05/2017") }, 
+   { Title: "Stengt", Date: new Date("03/05/2017") }];
+
+
+$("#datepicker").datepicker({
+
+
+beforeShowDay: function(date) {
+    var result = [true, '', null];
+    var matching = $.grep(events, function(event) {
+        return event.Date.valueOf() === date.valueOf();
+    });
+
+    if (matching.length) {
+        result = [true, 'highlight', null];
+    }
+    return result;
+}/*,
+
+onSelect: function(dateText) {
+    var date,
+        selectedDate = new Date(dateText),
+        i = 0,
+        event = null;*/
+
+    /* Determine if the user clicked an event: */
+ /*   while (i < events.length && !event) {
+        date = events[i].Date;
+
+        if (selectedDate.valueOf() === date.valueOf()) {
+            event = events[i];
+        }
+        i++;
+    }
+    if (event) {
+        /* If the event is defined, perform some action here; show a tooltip, navigate to a URL, etc. */
+    /*    alert(event.Title);
+    }
+
+}*/
+}); 
+
+//enddp
+}
+})
+}
+
+searchXML();
+
+// Make the gooogle maps map
 	function myMap(coordinate, text) {
 
 	  var map = new google.maps.Map(document.getElementById('map'), {
@@ -137,4 +136,4 @@ $(document).ready(function(){
 	});
 			
 });
-
+			
