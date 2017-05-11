662463e9f19c6d8b73c1705dc57ce7d04f8b7491
Espen K. Brun <espen_brun@hotmail.com>
2017-05-12 00:06:55 +0200

    form is filled with id and date, but readonly. date is weird format

--- skjema_bestilling.js
+++ skjema_bestilling.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 	$(".container-footer").load("footer.html");
 
-	var id = window.location.hash.substring(1);
+	var substring = window.location.hash.substring(1).split(':');
+	var id = substring[0].toString().trim();
+	var date = substring[1].toString().trim();
 	console.log('id'+id);
+	console.log('dato'+date);
 	$("[name=hytteid]").val(id);
+	$("[name=date]").val(date);
+
 });
