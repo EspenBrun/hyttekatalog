dffb74c61879c80506055b1cab7e78495a929eed
Espen K. Brun <espen_brun@hotmail.com>
2017-05-11 20:02:36 +0200

    Changes from anna. Eihytteliste all info rendered based on cabin chosen

--- eihytte.js
+++ eihytte.js
@@ -5,16 +5,16 @@
 
 $(document).ready(function(){
 	// Hent informasjon om hvilken type hytter som skal vises fra enden av urlen
-	var substringArray = window.location.hash.substring(1).split(';');
-	var hyttetype = substringArray[0];
-	var id = substringArray[1];
+	var substringArray = window.location.hash.substring(1).split(',');
+	var id = substringArray[0];
+	var hyttetype = substringArray[1];
 	var myXML;
 	var txt;
-	var result;
-	var header = id;
-	var test;
-	
-	$("#cabin-type-header").html(id);
+	var tur;
+	var info;
+	var varet;
+	var txtArr;
+	var events;
 		
 	// Hent informasjon om hytta
 	function searchXML(){
@@ -27,19 +27,84 @@ $(document).ready(function(){
 		myXML = $(xml).find(hyttetype).filter(function(){		
 		 return	$(this).find("hytteid").text()==id;		 
 		});
-		test = $(this).find("kjøkken").text();
+	
 		//lagrer hytte-info i display variabelen
 		txt = myXML.children().map(function(){
-		return this.tagName + '=' + $(this).text();
+		return $(this).text() + ',';		
 		}).get().join(' ');
-		
-		alert(txt);	
-		
-		$("#eihytte").html("<p>"+txt+"</p>");
-		
-			}						
-		});										
-	}			// Send HTMLen til dette elementet	
+				
+		txtArr = txt.split(',');
+		console.log('txtArr:' + txtArr);
+		info = txtArr[10].toString().trim();
+		console.log('info:' + info);
+		vaer = txtArr[14].toString().trim();
+		console.log('varet:' + varet);
+		tur = txtArr[15].toString().trim();
+		console.log('tur:' + tur);
+						
+		varet = '\n'+ '<iframe src="'+ vaer +'" width="468" height="290" frameborder="0" style="margin: 10px 0 10px 0" scrolling="no"></iframe>\n';
+ 		console.log('vaer:' + vaer);
+
+	$("#beskrivelse").load(info);
+		$("#varet").append(varet);
+
+$(".tur").click(function(e){
+		e.preventDefault(); // hindre default oppførsel for browseren ved klikk
+		window.location.href = tur;
+});
+//dp
+
+events = [ 
+    { Title: "Opptat", Date: new Date("05/05/2017") }, 
+   { Title: "Stengt", Date: new Date("03/05/2017") }];
+
+
+$("#datepicker").datepicker({
+
+
+beforeShowDay: function(date) {
+    var result = [true, '', null];
+    var matching = $.grep(events, function(event) {
+        return event.Date.valueOf() === date.valueOf();
+    });
+
+    if (matching.length) {
+        result = [true, 'highlight', null];
+    }
+    return result;
+}/*,
+
+onSelect: function(dateText) {
+    var date,
+        selectedDate = new Date(dateText),
+        i = 0,
+        event = null;*/
+
+    /* Determine if the user clicked an event: */
+ /*   while (i < events.length && !event) {
+        date = events[i].Date;
+
+        if (selectedDate.valueOf() === date.valueOf()) {
+            event = events[i];
+        }
+        i++;
+    }
+    if (event) {
+        /* If the event is defined, perform some action here; show a tooltip, navigate to a URL, etc. */
+    /*    alert(event.Title);
+    }
+
+}*/
+}); 
+
+//enddp
+}
+})
+}
+
 searchXML();
 			
 });
+
+
+					
--- hytteliste.js
+++ hytteliste.js
@@ -3,7 +3,6 @@
 // Espen Kirkesæther Brun, Anna Nikitina og Kathinka Hoyden
 ///////////////////////////////////////////////////////////////
 
-
 $(document).ready(function(){
 	// Hent informasjon om hvilken type hytter som skal vises fra enden av urlen
 	var hyttetype = window.location.hash.substring(1);
@@ -14,6 +13,7 @@ $(document).ready(function(){
 		$("#cabin-type-header").html(hyttetype);
 	}
 	
+	
 	// Hent informasjon om hyttene
 	$.ajax({
 		type:"GET",
@@ -22,52 +22,9 @@ $(document).ready(function(){
 
 		success: function(xml) {
 			$(xml).find(hyttetype).each(function(){
-			console.log("the id is:" + $(this).find("hytteid").text().trim());
-
-				// Bootstrap wrappers
-				txt += "<div class='row'>";
+			txt += "<div class='row'>";
 				txt += "<div class='col-xs-12 col-sm-6'>";
-				//
-				// Carousel
-				//
-				txt += "<div id='myCarousel" + $(this).find("hytteid").text().trim() + "' class='carousel slide' data-ride='carousel'>";
-				// Carousel indicators
-				txt += "<ol class='carousel-indicators'>";
-				txt += "<li data-target='#myCarousel" + $(this).find("hytteid").text().trim() + "' data-slide-to='0' class='active'></li>";
-				txt += "<li data-target='#myCarousel" + $(this).find("hytteid").text().trim() + "' data-slide-to='1' class='active'></li>";
-				txt += "<li data-target='#myCarousel" + $(this).find("hytteid").text().trim() + "' data-slide-to='2' class='active'></li>";
-				txt += "<li data-target='#myCarousel" + $(this).find("hytteid").text().trim() + "' data-slide-to='3' class='active'></li>";
-				txt += "</ol>";
-				// Wrapper for slides
-				txt += "<div class='carousel-inner'>";
-				// Slides
-				txt += "<div class='item active'>";
-				txt += "<img src='" + $(this).find("bilde").text()+ "' alt='Los Angeles'>";
-				txt += "</div>";
-				txt += "<div class='item'>";
-				txt += "<img src='" + $(this).find("bilde1").text()+ "' alt='Chicago'>";
-				txt += "</div>";
-				txt += "<div class='item'>";
-				txt += "<img src='" + $(this).find("bilde2").text()+ "' alt='Chicago'>";
-				txt += "</div>";
-				txt += "<div class='item'>";
-				txt += "<img src='" + $(this).find("bilde3").text()+ "' alt='New York'>";
-				txt += "</div>";
-				txt += "</div>";
-				// Left and right controls
-				txt += "<a class='left carousel-control' href='#myCarousel" + $(this).find("hytteid").text().trim() + "' data-slide='prev'>";
-				txt += "<span class='glyphicon glyphicon-chevron-left''></span>";
-				txt += "<span class='sr-only'>Previous</span>";
-				txt += "</a>";
-				txt += "<a class='right carousel-control' href='#myCarousel" + $(this).find("hytteid").text().trim() + "' data-slide='next'>";
-				txt +="<span class='glyphicon glyphicon-chevron-right'></span>";
-				txt += "<span class='sr-only'>Next</span>";
-				txt += "</a>";
-				txt += "</div>";
-				// txt += "<img src='" + $(this).find("bilde").text()+ "' class='img-responsive img-rounded' alt='Bilde av hytten'>";
-				//
-				// Description in bootstrap jumbotron
-				//
+				txt += "<img src='" + $(this).find("bilde").text()+ "' class='img-responsive img-rounded' alt='Bilde av hytten'>";
 				txt += "</div>";
 				txt += "<div class='col-xs-12 col-sm-6 pull-left jumbotron cabin-list-description match-parent'>";
 				txt += "Beliggenhet: " + $(this).find("beliggenhet").text();
@@ -86,32 +43,29 @@ $(document).ready(function(){
 				txt += "<br>";
 				txt += "Pris: " + $(this).find("pris").text();
 				txt += "<br>";
-				txt += "<a href='#' class='eihytte' id='" + $(this).find("hytteid").text().trim() + "'>";
-				txt += "Vil du vite mer?";
-				txt += "</a>";
-				txt += "</div>";
-				txt += "</div>";
-				txt += "<hr>"; 
+			    txt += "<a href='#' class='eihytte' id='" + $(this).find("hytteid").text() + "'>";
+			    txt += "Vil du vite mer?";
+			    txt += "</a>";
+			    txt += "</div>";
+			    txt += "</div>";
+			    txt += "<hr>"; 
+			    
 			});
-			// Send HTMLen til dette elementet
+				// Send HTMLen til dette elementet
 			$("#hytteliste").html(txt);
+									
 		}
+	
 	});
-
 	$('#hytteliste').on('click', '.eihytte', function (e) {
+	    console.log("yeahhhh!!!");
 	    e.preventDefault(); // hindre default oppførsel for browseren ved klikk
 		var hytteid = this.id;
-		window.location.href = 'eihytte.html#' + hyttetype + ';' + hytteid;
-	});
-
-	$('#hytteliste').on('click', '.carousel-control.left', function () {
-	  $('#myCarousel').carousel('prev');
-	});
-
-	$('#hytteliste').on('click', '.carousel-control.right', function () {
-	  $('#myCarousel').carousel('next');
-	});
-
-	$('.container-footer').load('footer.html');
+		var subArray = [hytteid, hyttetype]
+		window.location.href = 'eihytte.html#' + subArray;
+	});	
+		   
 });
+					
+
 					
