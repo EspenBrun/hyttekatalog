dd9929f0a237768b069e7d372671a7d8d43464a9
Espen K. Brun <espen_brun@hotmail.com>
2017-05-12 00:03:05 +0200

    brought back readonly in form. got lost in some merge

--- skjema_bestilling.js
+++ skjema_bestilling.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
 	$(".container-footer").load("footer.html");
+
+	var id = window.location.hash.substring(1);
+	console.log('id'+id);
+	$("[name=hytteid]").val(id);
 });
